0b13259315df97abe128de4a24005e707971ffe6
inad9300 <inad9300@gmail.com>
2020-05-01 10:52:20 +0200

    Move to webpack

---
+++ client/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/main.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        include: path.resolve(__dirname, 'src')
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'bin')
+  }
+}
