dfa8e99d65e205f621bf103e0622240c95dfab7e
inad9300 <inad9300@gmail.com>
2020-05-01 10:52:32 +0200

    Optimize FontAwesome imports

--- client/src/components/Icon.ts
+++ client/src/components/Icon.ts
@@ -1,9 +1,27 @@
 import { library, icon, findIconDefinition } from '@fortawesome/fontawesome-svg-core'
-import { faSeedling, faPlus, faBinoculars, faCalendarWeek, faUserCog, faQuestion } from '@fortawesome/free-solid-svg-icons'
+import { faBinoculars } from '@fortawesome/free-solid-svg-icons/faBinoculars'
+import { faCalendarWeek } from '@fortawesome/free-solid-svg-icons/faCalendarWeek'
+import { faPlus } from '@fortawesome/free-solid-svg-icons/faPlus'
+import { faQuestion } from '@fortawesome/free-solid-svg-icons/faQuestion'
+import { faSeedling } from '@fortawesome/free-solid-svg-icons/faSeedling'
+import { faUserCog } from '@fortawesome/free-solid-svg-icons/faUserCog'
 
-library.add(faSeedling, faPlus, faBinoculars, faCalendarWeek, faUserCog, faQuestion)
+library.add(
+  faBinoculars,
+  faCalendarWeek,
+  faPlus,
+  faQuestion,
+  faSeedling,
+  faUserCog,
+)
 
-export type IconName = 'seedling' | 'plus' | 'binoculars' | 'calendar-week' | 'user-cog' | 'question'
+export type IconName =
+  'binoculars' |
+  'calendar-week' |
+  'plus' |
+  'question' |
+  'seedling' |
+  'user-cog'
 
 export function Icon(name: IconName) {
   return icon(findIconDefinition({ prefix: 'fas', iconName: name })).node[0] as SVGSVGElement
