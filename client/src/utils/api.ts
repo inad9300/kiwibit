50becc5a424336db58b5d50dee12a244d1177195
inad9300 <inad9300@gmail.com>
2020-05-01 16:24:49 +0200

    Minor changes

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,16 +1,14 @@
 import type { Api } from '../../../server/src/Api'
 
-const headers = { 'Content-Type': 'application/json; charset=utf-8' }
-
 export function api<Fn extends keyof Api>(
   fn: Fn,
   payload: Parameters<Api[Fn]>[0]
 ): ReturnType<Api[Fn]> {
   return fetch('http://localhost:4000/api/' + fn, {
     method: 'POST',
-    headers,
+    headers: { 'Content-Type': 'application/json; charset=utf-8' },
     body: JSON.stringify(payload)
   })
-    .then(res => res.text())
-    .then(body => (body ? JSON.parse(body) : undefined)) as ReturnType<Api[Fn]>
+  .then(res => res.text())
+  .then(body => (body ? JSON.parse(body) : undefined)) as ReturnType<Api[Fn]>
 }
