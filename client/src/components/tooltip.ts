c8897b112d48e28f7468845c8647990ca441cf8d
inad9300 <inad9300@gmail.com>
2020-05-02 22:49:45 +0200

    Fix tooltip going out of screen

--- client/src/components/tooltip.ts
+++ client/src/components/tooltip.ts
@@ -3,27 +3,32 @@ tooltip.style.pointerEvents = 'none'
 tooltip.style.fontSize = '13px'
 tooltip.style.padding = '6px'
 tooltip.style.display = 'none'
+tooltip.style.width = 'max-content'
+tooltip.style.maxWidth = '300px'
 tooltip.style.position = 'fixed'
 tooltip.style.zIndex = '100'
 tooltip.style.color = '#fff'
 tooltip.style.borderRadius = '3px'
 tooltip.style.backgroundColor = 'rgba(0, 0, 0, 0.8)'
 
-// FIXME Can go out of screen!
-// TODO Add minWidth
+const { documentElement } = document
+const { min } = Math
 
 export function attachTooltip(content: string | HTMLElement, ref: HTMLElement) {
   ref.addEventListener('mouseenter', evt => {
     tooltip.innerHTML = ''
     tooltip.append(content)
-    reposition(evt)
     tooltip.style.display = 'block'
+    reposition(evt)
   })
   ref.addEventListener('mousemove', reposition)
   ref.addEventListener('mouseleave', () => tooltip.style.display = 'none')
 
   function reposition(evt: MouseEvent) {
-    tooltip.style.top = evt.clientY - 2 + 'px'
-    tooltip.style.left = evt.clientX + 16 + 'px'
+    const maxTop = documentElement.clientHeight - tooltip.offsetHeight
+    const maxLeft = documentElement.clientWidth - tooltip.offsetWidth
+
+    tooltip.style.top = min(maxTop, evt.clientY + 10) + 'px'
+    tooltip.style.left = min(maxLeft, evt.clientX + 10) + 'px'
   }
 }
