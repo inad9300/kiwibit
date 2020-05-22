50c915bc927406a67dc1dea65d2c3874290eb3fb
danielma <daniel.martin.anido@cern.ch>
2020-05-22 21:09:05 +0200

    Change logo font

--- client/src/header/Logo.ts
+++ client/src/header/Logo.ts
@@ -2,21 +2,24 @@ import { AppsMenu } from './AppsMenu'
 import { Html } from '../components/Html'
 import { BaseButton } from '../components/BaseButton'
 
+const height = '47px'
+
 export const logoTriangle = Html('div').with(it => {
-  it.style.borderTop = '47px solid transparent'
+  it.style.borderTop = height + ' solid transparent'
   it.style.borderRight = '20px solid transparent'
 })
 
 export function Logo() {
   const root = BaseButton().with(it => {
     it.textContent = 'meals'
+    it.style.height = height
     it.style.position = 'relative'
     it.style.margin = '0'
     it.style.padding = '10px 16px 10px 16px'
-    it.style.fontSize = '20px'
-    it.style.fontWeight = '600'
-    it.style.color = '#666'
-    it.style.textShadow = '0 -1px 0 rgba(255, 255, 255, 1)'
+    it.style.fontFamily = 'Pacifico, sans-serif'
+    it.style.fontSize = '24px'
+    it.style.lineHeight = '0'
+    it.style.textShadow = '0 -1px 0 rgba(255, 255, 255, 0.75)'
 
     it.onclick = () => menu.toggle()
     it.onmouseenter = it.onfocus = () => setActive()
@@ -31,12 +34,12 @@ export function Logo() {
   setInactive()
 
   function setActive() {
-    root.style.color = '#111'
+    root.style.color = '#333'
     root.style.backgroundColor = logoTriangle.style.borderTopColor = 'rgba(0, 0, 0, 0.09)'
   }
 
   function setInactive() {
-    root.style.color = '#666'
+    root.style.color = '#b12a42'
     root.style.backgroundColor = logoTriangle.style.borderTopColor = 'rgba(0, 0, 0, 0.04)'
   }
 
