1599780f6cbd693b3abd785bd17e96ffc69c1c7f
danielma <daniel.martin.anido@cern.ch>
2020-05-27 23:09:50 +0200

    Fix sometimes-non-circular circle

--- client/src/top-foods/CategoryCircle.ts
+++ client/src/top-foods/CategoryCircle.ts
@@ -2,7 +2,7 @@ import { Html } from '../components/Html'
 
 export function CategoryCircle(color: string) {
   return Html('div').with(it => {
-    it.style.width = it.style.height = it.style.borderRadius = '11px'
+    it.style.width = it.style.height = it.style.minWidth = it.style.minHeight = it.style.borderRadius = '11px'
     it.style.backgroundColor = color
   })
 }
