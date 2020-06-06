9f51dfb023c9ed5a06227cdb4647fc0d0d4c2843
danielma <daniel.martin.anido@cern.ch>
2020-06-06 20:37:44 +0200

    Create AbortablePromise type

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -8,6 +8,10 @@ export type ApiInput<Fn extends keyof Api> = Parameters<Api[Fn]>[0]
 
 export type ApiOutput<Fn extends keyof Api> = ThenArg<ReturnType<Api[Fn]>>
 
+export type AbortablePromise<T> = Promise<T> & {
+  abort(): void
+}
+
 export function api<Fn extends keyof Api>(fn: Fn, payload: ApiInput<Fn>) {
   const aborter = new AbortController()
 
