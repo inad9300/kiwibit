e1f5f287ffae19768ca1e4609d1c08c0e9caade1
danielma <daniel.martin.anido@cern.ch>
2020-06-07 10:54:43 +0200

    Remove unused functions

--- client/src/meal-plan/NutritionalOverview.ts
+++ client/src/meal-plan/NutritionalOverview.ts
@@ -1,12 +1,10 @@
-import { randomInt } from '../utils/randomInt'
-
 export function NutritionalOverview() {
   const root = document.createElement('div')
 
   for (let i = 0; i < 16; ++i) {
     const progressBar = document.createElement('progress')
     progressBar.max = 100
-    progressBar.value = randomInt(0, 100)
+    progressBar.value = 42
     progressBar.style.display = 'block'
     progressBar.style.marginBottom = '12px'
 
