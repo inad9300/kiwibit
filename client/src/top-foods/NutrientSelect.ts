153daf23c942da7ae227fc73a5d4b5c63ff015e2
danielma <daniel.martin.anido@cern.ch>
2020-06-11 21:20:27 +0200

    Eliminate unit from nutrient select

--- client/src/top-foods/NutrientSelect.ts
+++ client/src/top-foods/NutrientSelect.ts
@@ -5,7 +5,7 @@ export function NutrientSelect() {
   const select = Select<ApiOutput<'getAllNutrients'>[0]>(
     'Nutrient',
     n => n.id!,
-    n => n.name + (n.alias ? ' / ' + n.alias : '') + ' (' + n.unit_abbr + ')',
+    n => n.name + (n.alias ? ' / ' + n.alias : ''),
     true
   )
   .with(it => {
@@ -28,7 +28,7 @@ export function NutrientSelect() {
   })
 
   api('getAllNutrients', undefined).then(nutrients => {
-    select.setOptions(nutrients.sort((a, b) => (a.name > b.name ? 1 : -1)))
+    select.setOptions(nutrients.sort((a, b) => a.name > b.name ? 1 : -1))
     select.onReady()
   })
 
