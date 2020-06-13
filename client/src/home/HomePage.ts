aa65af169b5345aa9ca8c751ea8a4e7db0b3881f
danielma <daniel.martin.anido@cern.ch>
2020-06-13 18:33:20 +0200

    Remove placeholder text from home page

--- client/src/home/HomePage.ts
+++ client/src/home/HomePage.ts
@@ -3,6 +3,7 @@ import { LinkButton } from '../components/LinkButton'
 import { Page } from '../pages'
 import { Hbox } from '../components/Box'
 import { Spacer } from '../components/Spacer'
+import { Span } from '../components/Span'
 
 export function HomePage() {
   return Html('div').with(it => {
@@ -12,18 +13,16 @@ export function HomePage() {
     it.style.fontSize = '15px'
     it.style.color = '#333'
 
-    it.innerHTML = `
-      <h1>Meal Planning <span style="color: grey">and</span> Nutritional Tracking</h1>
-      <p>En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo que vivía un hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y galgo corredor.</p>
-      <p>Una olla de algo más vaca que carnero, salpicón las más noches, duelos y quebrantos los sábados, lantejas los viernes, algún palomino de añadidura los domingos, consumían las tres partes de su hacienda.</p>
-      <p>El resto della concluían sayo de velarte, calzas de velludo para las fiestas, con sus pantuflos de lo mesmo, y los días de entresemana se honraba con su vellorí de lo más fino.</p>
-      <p>Tenía en su casa una ama que pasaba de los cuarenta, y una sobrina que no llegaba a los veinte, y un mozo de campo y plaza, que así ensillaba el rocín como tomaba la podadera.</p>
-      <p>Frisaba la edad de nuestro hidalgo con los cincuenta años; era de complexión recia, seco de carnes, enjuto de rostro, gran madrugador y amigo de la caza.</p>
-      <p>Quieren decir que tenía el sobrenombre de Quijada, o Quesada, que en esto hay alguna diferencia en los autores que deste caso escriben; aunque por conjeturas verosímiles se deja entender que se llamaba Quijana.</p>
-      <p>Pero esto importa poco a nuestro cuento: basta que en la narración dél no se salga un punto de la verdad.</p>
-    `
-
     it.append(
+      Html('h1').with(it => {
+        it.append(
+          'Meal Planning',
+          Span(' and ').with(it => {
+            it.style.color = 'grey'
+          }),
+          'Nutritional Tracking'
+        )
+      }),
       LinedTitle('Explore foods').with(it => {
         it.style.marginBottom = '8px'
       }),
