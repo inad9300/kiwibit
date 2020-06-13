5473c5600cd2d79606ff1a68cac7fc8891f18ffd
danielma <daniel.martin.anido@cern.ch>
2020-06-13 11:34:05 +0200

    Complete settings page

---
+++ client/src/settings/SettingsApi.ts
@@ -0,0 +1,42 @@
+import { ApiOutput } from '../utils/api'
+
+interface Settings {
+  age: number
+  sex: 'M' | 'F'
+  food_categories: number[]
+  nutrients: number[]
+}
+
+export async function saveSettings(s: Settings) {
+  localStorage.setItem('settings', JSON.stringify(s))
+}
+
+export async function fetchSettings(
+  foodCategories: ApiOutput<'getAllUsdaCategories'>,
+  nutrients: ApiOutput<'getAllNutrients'>
+): Promise<Settings> {
+  const s = localStorage.getItem('settings')
+  if (s) {
+    return JSON.parse(s);
+  }
+
+  return {
+    age: 25,
+    sex: 'M',
+    food_categories: foodCategories.filter(c => c.is_visible_default).map(c => c.id),
+    nutrients: nutrients.filter(n => n.is_visible_default).map(n => n.id)
+  }
+}
+
+export async function fetchAgeAndSexSettings() {
+  const { age, sex } = await fetchSettings([], [])
+  return { age, sex }
+}
+
+export async function fetchFoodCategoriesSettings(foodCategories: ApiOutput<'getAllUsdaCategories'>) {
+  return (await fetchSettings(foodCategories, [])).food_categories
+}
+
+export async function fetchNutrientsSettings(nutrients: ApiOutput<'getAllNutrients'>) {
+  return (await fetchSettings([], nutrients)).nutrients
+}
--- client/src/settings/SettingsPage.ts
+++ client/src/settings/SettingsPage.ts
@@ -7,35 +7,59 @@ import { api } from '../utils/api'
 import { Fieldset } from '../components/Fieldset'
 import { groupBy } from '../utils/groupBy'
 import { LabeledCheckbox } from '../components/LabeledCheckbox'
+import { fetchSettings, saveSettings } from './SettingsApi'
 
 export function SettingsPage() {
   return Html('div').with(it => {
     it.style.padding = '12px 16px 16px'
 
-    const foodCategoriesBox = Fieldset('Food Categories').with(it => {
-      it.style.marginTop = '10px'
+    const ageTextField = TextField().with(it => {
+      it.type = 'number'
+      it.min = '0'
+      it.max = '122'
     })
 
-    api('getAllUsdaCategories', undefined).then(categories => {
-      categories
-        .sort((a, b) => a.name > b.name ? 1 : -1)
-        .map(c => {
-          return Hbox().with(it => {
-            it.append(
-              LabeledCheckbox(c.name).with(it => {
-                it.checkbox.checked = c.is_visible_default
-              })
-            )
-          })
-        })
-        .forEach(c => foodCategoriesBox.append(c))
+    const sexSelect = Select<string>('Sex', s => s.slice(0, 1), s => s).with(it => {
+      it.setOptions(['Male', 'Female'])
+    })
+
+    const foodCategoriesBox = Fieldset('Food Categories').with(it => {
+      it.style.marginTop = '10px'
     })
 
     const nutrientsBox = Fieldset('Nutrients').with(it => {
       it.style.marginTop = '10px'
     })
 
-    api('getAllNutrients', undefined).then(nutrients => {
+    const foodCategoryCheckboxes: HTMLInputElement[] = []
+    const nutrientCheckboxes: HTMLInputElement[] = []
+
+    Promise.all([
+      api('getAllUsdaCategories', undefined),
+      api('getAllNutrients', undefined)
+    ])
+    .then(async ([foodCategories, nutrients]) => {
+      const settings = await fetchSettings(foodCategories, nutrients)
+      return [foodCategories, nutrients, settings] as const
+    })
+    .then(([foodCategories, nutrients, settings]) => {
+      ageTextField.value = '' + settings.age
+      sexSelect.select.value = settings.sex
+
+      foodCategories.sort((a, b) => a.name > b.name ? 1 : -1).forEach(c => {
+        const labeledCheckbox = LabeledCheckbox(c.name).with(it => {
+          it.checkbox.dataset.id = '' + c.id
+          it.checkbox.checked = settings.food_categories.includes(c.id)
+        })
+
+        foodCategoryCheckboxes.push(labeledCheckbox.checkbox)
+        foodCategoriesBox.append(
+          Hbox().with(it => {
+            it.append(labeledCheckbox)
+          })
+        )
+      })
+
       const groupedNutrients = groupBy(nutrients, 'category')
       Object.keys(groupedNutrients).forEach(group => {
         const groupWrapper = Html('div').with(it => {
@@ -51,51 +75,44 @@ export function SettingsPage() {
             it.style.borderBottom = '1px solid #aaa'
           })
         )
-        groupedNutrients[group]
-          .map(n => {
-            return Hbox().with(it => {
-              it.style.marginLeft = '4px'
-              it.append(
-                LabeledCheckbox(n.name + (n.alias ? ' / ' + n.alias : '')).with(it => {
-                  it.checkbox.checked = n.is_visible_default
-                })
-              )
-            })
+        groupedNutrients[group].forEach(n => {
+          const labeledCheckbox = LabeledCheckbox(n.name + (n.alias ? ' / ' + n.alias : '')).with(it => {
+            it.checkbox.dataset.id = '' + n.id
+            it.checkbox.checked = settings.nutrients.includes(n.id)
           })
-          .forEach(n => groupWrapper.append(n))
+
+          nutrientCheckboxes.push(labeledCheckbox.checkbox)
+          groupWrapper.append(
+            Hbox().with(it => {
+              it.style.marginLeft = '4px'
+              it.append(labeledCheckbox)
+            })
+          )
+        })
 
         nutrientsBox.append(groupWrapper)
+      })
+
+      it.oninput = () => saveSettings({
+        age: parseInt(ageTextField.value, 10),
+        sex: sexSelect.getSelected()!.slice(0, 1) as 'M' | 'F',
+        food_categories: foodCategoryCheckboxes.filter(c => c.checked).map(c => parseInt(c.dataset.id!, 10)),
+        nutrients: nutrientCheckboxes.filter(c => c.checked).map(c => parseInt(c.dataset.id!, 10))
       })
     })
 
     it.append(
       Hbox().with(it => {
-        it.append(
+        it.setChildren([
           Vbox().with(it => {
-            it.style.marginRight = '8px'
             it.style.minWidth = '80px'
-            it.append(
-              ControlTitle('Age'),
-              TextField().with(it => {
-                it.type = 'number'
-                it.min = '0'
-                it.max = '122'
-                it.value = '25'
-              })
-            )
+            it.append(ControlTitle('Age'), ageTextField)
           }),
           Vbox().with(it => {
             it.style.minWidth = '80px'
-            it.append(
-              Select<string>('Sex', s => s, s => s).with(it => {
-                it.setOptions([
-                  'Male',
-                  'Female'
-                ])
-              })
-            )
+            it.append(sexSelect)
           })
-        )
+        ], '8px')
       }),
       foodCategoriesBox,
       nutrientsBox
