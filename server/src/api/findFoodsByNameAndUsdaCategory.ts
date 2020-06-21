660015d0fc20eecfa1665d902bdb4af5eb981ac4
danielma <daniel.martin.anido@cern.ch>
2020-06-21 15:20:25 +0200

    Improve food typeahead search query performance

--- server/src/api/findFoodsByNameAndUsdaCategory.ts
+++ server/src/api/findFoodsByNameAndUsdaCategory.ts
@@ -11,18 +11,15 @@ export async function findFoodsByNameAndUsdaCategory(data: {
   usdaCategoryIds: number[]
 }) {
   const res = await pool.query<Food>(`
+    with foods_in_cat as (
+      select f.id, f.name, f.name_tokens
+      from foods f
+      where ($1::int[] is null or f.usda_category_id = any($1))
+    )
     (
-      (select f.id, f.name
-      from foods f
-      where ($1::int[] is null or f.usda_category_id = any($1))
-      and f.name_tokens @@ websearch_to_tsquery($2)
-      order by f.name)
-    union
-      (select f.id, f.name
-      from foods f
-      where ($1::int[] is null or f.usda_category_id = any($1))
-      and lower(f.name) like '%' || lower($3) || '%'
-      order by f.name)
+      select fc.id, fc.name from foods_in_cat fc where fc.name_tokens @@ websearch_to_tsquery($2)
+      union
+      select fc.id, fc.name from foods_in_cat fc where lower(fc.name) like '%' || lower($3) || '%'
     )
     limit 100
   `, [
