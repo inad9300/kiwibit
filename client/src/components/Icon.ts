cf710ed6ba7b8f39ab1cfe397fac8e5076d7ea8e
danielma <daniel.martin.anido@cern.ch>
2020-07-01 22:02:30 +0200

    Sort icon imports alphabetically

--- client/src/components/Icon.ts
+++ client/src/components/Icon.ts
@@ -1,39 +1,39 @@
 import { library, icon, findIconDefinition } from '@fortawesome/fontawesome-svg-core'
 import { faBinoculars } from '@fortawesome/free-solid-svg-icons/faBinoculars'
 import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons/faCalendarAlt'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons/faChevronDown'
+import { faHome } from '@fortawesome/free-solid-svg-icons/faHome'
+import { faMugHot } from '@fortawesome/free-solid-svg-icons/faMugHot'
 import { faPlus } from '@fortawesome/free-solid-svg-icons/faPlus'
 import { faQuestion } from '@fortawesome/free-solid-svg-icons/faQuestion'
+import { faRulerCombined } from '@fortawesome/free-solid-svg-icons/faRulerCombined'
 import { faSeedling } from '@fortawesome/free-solid-svg-icons/faSeedling'
 import { faUserCog } from '@fortawesome/free-solid-svg-icons/faUserCog'
-import { faHome } from '@fortawesome/free-solid-svg-icons/faHome'
-import { faChevronDown } from '@fortawesome/free-solid-svg-icons/faChevronDown'
-import { faMugHot } from '@fortawesome/free-solid-svg-icons/faMugHot'
-import { faRulerCombined } from '@fortawesome/free-solid-svg-icons/faRulerCombined'
 
 library.add(
   faBinoculars,
   faCalendarAlt,
+  faChevronDown,
+  faHome,
+  faMugHot,
   faPlus,
   faQuestion,
+  faRulerCombined,
   faSeedling,
-  faUserCog,
-  faHome,
-  faChevronDown,
-  faMugHot,
-  faRulerCombined
+  faUserCog
 )
 
 export type IconName =
   'binoculars' |
   'calendar-alt' |
+  'chevron-down' |
+  'home' |
+  'mug-hot' |
   'plus' |
   'question' |
+  'ruler-combined' |
   'seedling' |
-  'user-cog' |
-  'home' |
-  'chevron-down' |
-  'mug-hot' |
-  'ruler-combined'
+  'user-cog'
 
 export function Icon(name: IconName) {
   return icon(findIconDefinition({ prefix: 'fas', iconName: name })).node[0] as SVGSVGElement
