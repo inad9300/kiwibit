cd5b1aff4ff0bf39a35c81ba72036d7f44ab722b
danielma <daniel.martin.anido@cern.ch>
2020-07-05 17:20:46 +0200

    Add Backdrop component for modals

---
+++ client/src/components/Backdrop.ts
@@ -0,0 +1,16 @@
+import { Html } from './Html'
+
+export function Backdrop() {
+  return Html('div').with(it => {
+    it.hidden = true
+    it.onclick = () => it.hidden = true
+    it.style.display = 'flex'
+    it.style.justifyContent = 'center'
+    it.style.width = it.style.height = '100%'
+    it.style.padding = '32px'
+    it.style.position = 'fixed'
+    it.style.zIndex = '2'
+    it.style.top = it.style.left = '0'
+    it.style.backgroundColor = 'rgba(0, 0, 0, 0.32)'
+  })
+}
